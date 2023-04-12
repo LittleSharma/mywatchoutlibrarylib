d1110c468790598a434f6b2262e0e78f9f7000ac
LittleSharma <little.sharma@aryavratinfotech.com>
2023-04-12 14:31:14 +0530

    second commit

--- app/src/main/java/com/example/watchoutemergencylib/MainActivity.kt
+++ app/src/main/java/com/example/watchoutemergencylib/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.watchoutemergency.Controller.WatchOutEmergency
 import com.example.watchoutemergencylib.databinding.ActivityMainBinding
